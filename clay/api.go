a322357d38ad95e5883f985204c0f5d1994d1de5
Jaco Labuschagne <zodimo@gmail.com>
2025-11-12 15:37:54 +0200

    clay id

--- clay/api.go
+++ clay/api.go
@@ -21,3 +21,12 @@ func CLAY_STRING(label string) Clay_String {
 		Chars:                 []byte(label),
 	}
 }
+
+// Note: If a compile error led you here, you might be trying to use CLAY_ID with something other than a string literal. To construct an ID with a dynamic string, use CLAY_SID instead.
+func CLAY_ID(label string) Clay_ElementId {
+	return CLAY_SID(CLAY_STRING(label))
+}
+
+func CLAY_SID(label Clay_String) Clay_ElementId {
+	return Clay__HashString(label, 0)
+}

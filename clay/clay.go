ac73b9367dc31bb822bddd9284980aae53cc9eb4
Jaco Labuschagne <zodimo@gmail.com>
2025-11-12 06:37:21 +0200

    mem and extract context to own file

--- clay/clay.go
+++ clay/clay.go
@@ -1,8 +1,6 @@
 package clay
 
-import (
-	"github.com/zodimo/go-arena-memory/mem"
-)
+import "github.com/zodimo/clay-go/pkg/mem"
 
 // CLAY_DLL_EXPORT Clay_Context* Clay_Initialize(Clay_Arena arena, Clay_Dimensions layoutDimensions, Clay_ErrorHandler errorHandler);
 
@@ -430,77 +428,6 @@ type Clay__ScrollContainerDataInternal struct {
 	ElementId           uint32
 	OpenThisFrame       bool
 	PointerScrollActive bool
-}
-
-type Clay_Context struct {
-	MaxElementCount              int32
-	MaxMeasureTextCacheWordCount int32
-	WarningsEnabled              bool
-	ErrorHandler                 Clay_ErrorHandler
-
-	BooleanWarnings Clay_BooleanWarnings
-	Warnings        *Clay__Array[Clay__Warning]
-
-	PointerInfo      Clay_PointerData
-	LayoutDimensions Clay_Dimensions
-
-	DynamicElementIndexBaseHash Clay_ElementId
-	DynamicElementIndex         int32
-
-	DebugModeEnabled              bool
-	DisableCulling                bool
-	ExternalScrollHandlingEnabled bool
-
-	DebugSelectedElementId uint32
-	Generation             uint32
-	ArenaResetOffset       uintptr
-
-	MeasureTextUserData       interface{}
-	QueryScrollOffsetUserData interface{}
-
-	InternalArena Clay_Arena
-
-	// Layout Elements / Render Commands
-	LayoutElements              *Clay__Array[Clay_LayoutElement]
-	RenderCommands              *Clay__Array[Clay_RenderCommand]
-	OpenLayoutElementStack      *Clay__Array[int32]
-	LayoutElementChildren       *Clay__Array[int32]
-	LayoutElementChildrenBuffer *Clay__Array[int32]
-	TextElementData             *Clay__Array[Clay__TextElementData]
-	AspectRatioElementIndexes   *Clay__Array[int32]
-	ReusableElementIndexBuffer  *Clay__Array[int32]
-	LayoutElementClipElementIds *Clay__Array[int32]
-
-	// Configs*
-	LayoutConfigs             *Clay__Array[Clay_LayoutConfig]
-	ElementConfigs            *Clay__Array[Clay_ElementConfig]
-	TextElementConfigs        *Clay__Array[Clay_TextElementConfig]
-	AspectRatioElementConfigs *Clay__Array[Clay_AspectRatioElementConfig]
-	ImageElementConfigs       *Clay__Array[Clay_ImageElementConfig]
-	FloatingElementConfigs    *Clay__Array[Clay_FloatingElementConfig]
-	ClipElementConfigs        *Clay__Array[Clay_ClipElementConfig]
-	CustomElementConfigs      *Clay__Array[Clay_CustomElementConfig]
-	BorderElementConfigs      *Clay__Array[Clay_BorderElementConfig]
-	SharedElementConfigs      *Clay__Array[Clay_SharedElementConfig]
-
-	// Misc Data Structures
-	LayoutElementIdStrings             *Clay__Array[Clay_String]
-	WrappedTextLines                   *Clay__Array[Clay__WrappedTextLine]
-	LayoutElementTreeNodeArray1        *Clay__Array[Clay_LayoutElementTreeNode]
-	LayoutElementTreeRoots             *Clay__Array[Clay__LayoutElementTreeRoot]
-	LayoutElementsHashMapInternal      *Clay__Array[Clay_LayoutElementHashMapItem]
-	LayoutElementsHashMap              *Clay__Array[int32]
-	MeasureTextHashMapInternal         *Clay__Array[Clay__MeasureTextCacheItem]
-	MeasureTextHashMapInternalFreeList *Clay__Array[int32]
-	MeasureTextHashMap                 *Clay__Array[int32]
-	MeasuredWords                      *Clay__Array[Clay__MeasuredWord]
-	MeasuredWordsFreeList              *Clay__Array[int32]
-	OpenClipElementStack               *Clay__Array[int32]
-	PointerOverIds                     *Clay__Array[Clay_ElementId]
-	ScrollContainerDatas               *Clay__Array[Clay__ScrollContainerDataInternal]
-	TreeNodeVisited                    *Clay__Array[bool]
-	DynamicStringData                  *Clay__Array[byte] // char
-	DebugElementData                   *Clay__Array[Clay__DebugElementData]
 }
 
 type Clay_Padding struct {
